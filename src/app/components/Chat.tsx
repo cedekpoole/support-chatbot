9d9892bbf6a7a7174c3dd7e294c0bee39f728ea5
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-05-04 18:48:32 +0100

    install shadcn ui component

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,7 +1,12 @@
+import { Accordion, AccordionItem } from '@/components/ui/accordion'
 import { FC } from 'react'
 
 const Chat: FC = () => {
-  return <div>Chat</div>
+  return <Accordion type="single" collapsible className="relative bg-white z-40 shadow">
+    <AccordionItem value="item-1">
+        <div className="fixed right-8 w-80 bottom-8">Hello</div>
+    </AccordionItem>
+  </Accordion>
 }
 
 export default Chat
