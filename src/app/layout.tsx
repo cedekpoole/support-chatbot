32917b882dcf603cddf50db1ad6fe3f965b84e37
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-05-04 22:10:19 +0100

    create + set up chat input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import Chat from "./components/Chat";
+import Providers from "./components/Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'OpenAI Chatbot',
-  description: 'A chatbot that uses the OpenAI API to generate text',
-}
+  title: "OpenAI Chatbot",
+  description: "A chatbot that uses the OpenAI API to generate text",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Chat />
-        {children}
+      <Providers>
+        <body className={inter.className}>
+          <Chat />
+          {children}
         </body>
+      </Providers>
     </html>
-  )
+  );
 }
